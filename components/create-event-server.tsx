3dac2be1f8fd39e1466fdcb23516ce91b43cf9cd
c47s <36133719+c47s@users.noreply.github.com>
2024-11-11 19:21:57 -0500

    Connect Event Form to Database

--- components/create-event-server.tsx
+++ components/create-event-server.tsx
@@ -17,10 +17,19 @@ export async function printUser(){
     }
 } 
 
-async function submitEvent(event_name: string, event_location: string, event_description: string, event_start: Date, event_end: Date, event_host: string) {
+export async function submitEvent(event_name: string, event_location: string, event_description: string, event_start: Date, event_end: Date, event_host: number | null) {
     const { data: {user}} = await createClient().auth.getUser()
     if (user == null){
         throw new Error("You are not logged in!")
     }
-    const { error } = await supabase.from("events").insert({name: event_name, location: event_location, description: event_description, start:event_start, end:event_end, host_org: event_host, creator_id: user.id})
+    const { error } = await supabase.from("events")
+        .insert({
+            name: event_name,
+            location: event_location,
+            description: event_description,
+            start:event_start, end:event_end,
+            host_org: event_host,
+            creator_id: null /*user.id*/
+        })
+    return error;
 }
