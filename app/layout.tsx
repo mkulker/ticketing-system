63795279c708288f4b445a53ff5f318b16c863a8
PhilipShoebuckle <okodavali@umass.edu>
2024-11-12 22:17:02 -0500

    buy tickets

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,6 +39,7 @@ export default function RootLayout({
                   <div className="flex gap-5 items-center font-semibold">
                     <Link href={"/"}>Home</Link>
                     <Link href="/venue">Venue Page</Link>
+                    <Link href="/buyTickets">Buy Tickets</Link>
                   </div>
                   {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
                 </div>
