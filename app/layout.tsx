3a389243ea04f56ef66f9b9526032bdf83682de8
mkulker <mkulker@umass.edu>
2024-12-04 15:26:35 -0500

    fixed theme issues and added calendar to the nav

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,8 +43,10 @@ export default async function RootLayout({
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                   <div className="flex gap-5 items-center font-semibold">
                     <Link href={"/"}>Home</Link>
+                    <Link href="/calendar">Calendar</Link>
                     {user && <Link href="/venue"> Venue Page</Link>}
                     {user && <Link href={`/myTickets/${user.id}`}> My Tickets</Link>}
+                    <ThemeSwitcher />
                   </div>
                   {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
 
@@ -54,20 +56,6 @@ export default async function RootLayout({
                 {children}
               </div>
 
-              <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-                <p>
-                  Powered by{" "}
-                  <a
-                    href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-                    target="_blank"
-                    className="font-bold hover:underline"
-                    rel="noreferrer"
-                  >
-                    Supabase
-                  </a>
-                </p>
-                <ThemeSwitcher />
-              </footer>
             </div>
           </main>
         </ThemeProvider>
