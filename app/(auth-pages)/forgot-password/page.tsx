2351fd2a74ce24ea9498a973ebc622b5665d3894
mkulker <mkulker@umass.edu>
2024-12-08 18:16:37 -0500

    interface missing deploy fail fix

--- app/(auth-pages)/forgot-password/page.tsx
+++ app/(auth-pages)/forgot-password/page.tsx
@@ -6,11 +6,11 @@ import { Label } from "@/components/ui/label";
 import Link from "next/link";
 import { SmtpMessage } from "../smtp-message";
 
-export default function ForgotPassword({
-  searchParams,
-}: {
+interface ForgotPasswordProps {
   searchParams: Message;
-}) {
+}
+
+export default function ForgotPassword({ searchParams }: ForgotPasswordProps) {
   return (
     <>
       <form className="flex-1 flex flex-col w-full gap-2 text-foreground [&>input]:mb-6 min-w-64 max-w-64 mx-auto">
