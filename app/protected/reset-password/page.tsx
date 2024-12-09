9b486b956effdaecc4f64ea4c17c33f1c163b0a9
mkulker <mkulker@umass.edu>
2024-12-08 20:15:11 -0500

    interface missing deploy fail fix this time def works5

--- app/protected/reset-password/page.tsx
+++ app/protected/reset-password/page.tsx
@@ -1,14 +1,9 @@
 import { resetPasswordAction } from "@/app/actions";
-import { FormMessage, Message } from "@/components/form-message";
 import { SubmitButton } from "@/components/submit-button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
-export default async function ResetPassword({
-  searchParams,
-}: {
-  searchParams: Message;
-}) {
+export default async function ResetPassword() {
   return (
     <form className="flex flex-col w-full max-w-md p-4 gap-2 [&>input]:mb-4">
       <h1 className="text-2xl font-medium">Reset password</h1>
@@ -32,7 +27,6 @@ export default async function ResetPassword({
       <SubmitButton formAction={resetPasswordAction}>
         Reset password
       </SubmitButton>
-      <FormMessage message={searchParams} />
     </form>
   );
 }
